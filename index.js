f2001022f41abc37ebdeb799b23b864f9c00f56f
Daiki <hothanhhung131@gmail.com>
2024-05-27 16:11:04 +0700

    --no-edit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
 const app = express();
 app.use(bodyParser.json());
 
-// Cấu hình CORS middleware để chấp nhận tất cả các yêu cầu
+// Cấu hình CORS middleware
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE']
@@ -17,19 +17,15 @@ app.use(cors({
 // Sử dụng các routes
 app.use('/', (req, res) => {
   res.send('Hello World');
-});
-app.use('/api', routes);
+})
+app.use('/api',routes);
 
 // Middleware xử lý lỗi
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-const PROTOCOL = process.env.PROTOCOL || 'http';
-const PUBLIC_IP = '54.196.177.120'; // Địa chỉ IP của bạn
-
 app.listen(PORT, async () => {
-  const fullUrl = `${PROTOCOL}://${PUBLIC_IP}:${PORT}`;
-  console.log(`Server is running on ${fullUrl}`);
+  console.log(`Server is running on port ${PORT}`);
   await sequelize.sync();
   console.log('Database synchronized');
 });
